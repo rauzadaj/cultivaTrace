01b19a53b4d1e540a98b2e0f22be09632635a4c2
Jonathan RAUZADA <jonathan.rauzada@epitech.eu>
2026-02-24 12:04:28 +0100

    FIX : phpunit.xml.dist and .env.test added for testing

---
+++ tests/SmokeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testProjectBootstraps(): void
+    {
+        self::assertTrue(true);
+    }
+}
