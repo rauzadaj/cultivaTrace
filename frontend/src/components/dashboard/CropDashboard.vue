e88028909726bc40dbc9cb75e4eaeed13c29b4d1
Jonathan RAUZADA <jonathan.rauzada@epitech.eu>
2026-03-12 13:39:04 +0100

    fix(dashboard): replace deprecated vuetify nav prop

--- frontend/src/components/dashboard/CropDashboard.vue
+++ frontend/src/components/dashboard/CropDashboard.vue
@@ -24,7 +24,7 @@
           :subtitle="item.subtitle"
           :to="{ name: item.to }"
           rounded="lg"
-          active-color="white"
+          color="white"
         />
       </v-list>
 
