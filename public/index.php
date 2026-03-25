625115e081de6ae506983de031cf7f35f9cb8e61
Jonathan Rauzada <jrauzada@outlook.com>
2026-03-25 15:03:44 +0100

    fix(infra): make railway prod boot without .env file

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 use App\Kernel;
 
+if (!is_file(dirname(__DIR__).'/.env')) {
+    $_SERVER['APP_RUNTIME_OPTIONS']['disable_dotenv'] = true;
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
