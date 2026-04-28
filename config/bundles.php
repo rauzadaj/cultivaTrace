00f8e0d2d8ea9ed8d470c98776cdd9036e7fdb75
Jonathan RAUZADA <jonathan.rauzada@epitech.eu>
2026-04-28 12:03:13 +0200

    fix(ENV-F3): sync composer lock after partial sentry install

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Sensiolabs\GotenbergBundle\SensiolabsGotenbergBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
